60b1f3b02811329b6b506c669146d617f6fb9a43
Mário Lúcio <marioluciofjr@gmail.com>
2025-05-25 19:11:19 -0300

    Add files via upload

---
+++ mcp_news/server.py
@@ -0,0 +1,69 @@
+# server.py
+
+from mcp.server.fastmcp import FastMCP 
+from typing import Annotated
+from pydantic import Field
+import os
+from dotenv import load_dotenv
+from smolagents import WebSearchTool
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+senha_correta = os.getenv("SENHA")
+
+# Inicializa o servidor FastMCP 
+mcp = FastMCP("news")
+
+@mcp.tool(name="search_news")
+def search_news(pesquisa: str, senha: str):
+	"""
+	Ferramenta protegida por senha. Se a senha estiver correta, o resultado será mostrado.
+	"""
+	search_tool = WebSearchTool()
+      
+	if senha != senha_correta:
+		resultado = "Erro: Senha incorreta ou não fornecida."
+	else:
+		# Ação protegida
+		resultado = search_tool(f"{pesquisa}")
+	return resultado
+
+@mcp.prompt()
+def acesso(
+	senha: Annotated[str, Field(description="""Digite a senha correta para acessar a tool""")], 
+	tema: Annotated[str, Field(description="""Digite o tema da pesquisa""")]) -> str:
+	"""
+	Prompt que libera o acesso para utilizar a tool 'search_news'. Tem como argumentos a senha, que é alfanumérica e deve 
+	ser digitada corretamente e o tema de pesquisa.
+	"""
+
+	prompt = f"""
+	<função>
+	Você é uma jornalista experiente que sabe apurar bem uma notícia. Você não cai em fake news e sabe exatamente o valor de noticiar 
+	algo com embasamento. Você entende a importância de manter sua credibilidade jornalística.
+	</função>
+
+	<contexto>
+	Você só exercerá seu trabalho se a senha definida como {senha} estiver correta na tool 'search_news'.
+	</contexto>
+
+	<tarefa>
+	Foque sua pesquisa mesclando a palavra-chave definida por {tema} + a palavra 'news'. Salve isso em um placeholder chamado 'pesquisa'.
+	Esse placeholder será o argumento na tool 'search_news'.
+
+	A partir das informações coletadas pela tool 'search_news', você criará um resumo com o seguinte formato:
+
+	# Título da notícia
+
+	parágrafo 1: Resumo da notícia, incluindo os principais pontos e informações relevantes.
+	parágrafo 2: Contexto adicional, como a importância da notícia e possíveis implicações futuras.
+
+	</tarefa>
+	"""
+
+	return prompt
+
+if __name__ == "__main__":
+    mcp.run(transport='stdio')
+
